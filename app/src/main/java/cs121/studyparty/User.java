c6557e41490f7a9c0fa72161efcd52856f4a7bf4
lothomas17 <lothomas@students.pitzer.edu>
2015-09-29 23:36:21 -0700

    Finished up the basic functionality of the user class. It should be viable for the MVP.

--- app/src/main/java/cs121/studyparty/User.java
+++ app/src/main/java/cs121/studyparty/User.java
@@ -4,11 +4,22 @@
  */
 package cs121.studyparty;
 
+import java.util.Date;
 
 public class User {
 
-    public String name_;
-    public double timeFromLogin_;
+    //fields for User, just a name and the time since the user logged in.
+    private String name_;
+    private long timeFromLogin_;
+    private Date instantiatedAt_;
+
+    User(String userName){
+        setName(userName);
+
+        //sets the time of instantiation, making it easier to computer time.
+        long msTime = System.currentTimeMillis();
+        instantiatedAt_ = new Date(msTime);
+    }
 
     //Setters and Getters
 
@@ -20,8 +31,32 @@ public class User {
         name_ = name;
     }
 
+    /**
+     * Getter for the name field of the user class
+     * @return the name of the user
+     */
     public String getName() {
         return name_;
+    }
+
+    /**
+     * Setter for time, following procedure from:
+     * http://stackoverflow.com/questions/3352031/calculate-time-between-two-times-android
+     */
+    public void setTime() {
+        //gets the current time, and subtracts the login time from it.
+        long msTime = System.currentTimeMillis();
+        Date curDateTime = new Date(msTime);
+
+        timeFromLogin_ = curDateTime.getTime() - instantiatedAt_.getTime();
+    }
+
+    /**
+     * Getter for time field of the user class
+     * @return the time since the user checked into the study room.
+     */
+    public double getTime() {
+        return timeFromLogin_;
     }
 
 
